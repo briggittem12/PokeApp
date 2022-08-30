8ac6c8dc781c87a24e93a0a9563ab0e55a314d8a
Briggitte Merchan <briggittemerchan6@gmail.com>
2022-08-30 17:16:42 -0500

    im soooo tiredddd

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import axios from 'axios' 
+import './styles/pokedetll.css'
 
 const PokemonDetails = () => {
   const [pokemon, setPokemon] = useState()
@@ -15,9 +16,30 @@ const PokemonDetails = () => {
     }, [])
 
   return (
-    <article>
-      <h1>{name}</h1>
-      
+    <article className={`poke_uni bg-${pokemon?.types[0].type.name}`}>
+      <h1>{ name }</h1>
+      <div>
+        <img src={pokemon?.sprites.other["official-artwork"]["front_default"]} alt={pokemon?.name} />
+      </div>
+      <div >
+        <ul className='poke_control'>
+          <p>Type:</p>
+        {
+          pokemon?.types.map(slot => (
+            <li key={slot.type.url}>{slot.type.name}</li>
+            ))
+          }
+          <p>Stats:</p>
+        {
+          pokemon?.stats.map(cont => (
+            <p key={cont.stat.url}>
+              {cont.stat.name}{cont.base_stat}
+            </p>
+            ))
+          }
+          </ul>  
+        </div>
+
     </article>
     
   )
